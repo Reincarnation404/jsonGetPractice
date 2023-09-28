61fec1d77570777a710fcef102c38f13e7e277f3
INWELLCOM\tiffany.lin <tiffany.lin@ennowell.com>
2023-09-28 17:25:27 +0800

    資料顯示畫面完成 & 新增用gson處理JSON資料

--- app/src/main/java/com/example/jsonpractice/MainActivity.java
+++ app/src/main/java/com/example/jsonpractice/MainActivity.java
@@ -18,6 +18,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.jsonpractice.databinding.ActivityMainBinding;
+import com.example.jsonpractice.model.myData;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,9 +30,10 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.List;
 
 
 import okhttp3.Call;
@@ -41,10 +46,6 @@ import okhttp3.logging.HttpLoggingInterceptor;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-    //存放json檔的資料
-    ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
-
-    //Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,102 +53,139 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        catch_data();
+
+        binding.btnTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                catch_data();
+            }
+        });
+
+        binding.btnGson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                catch_data2();
+            }
+        });
+
 
     }
 
     private void catch_data(){
         String data = "https://jsonplaceholder.typicode.com/posts";
+
+        //網路接收資料是耗時操作
+        //
+        //因此在程式碼內必須寫入一些耗時執行緒的操作
+        //
+        //像是繼承Thread，或是Handler，或者是AsyneTask
+        //這裡是Thread搭配runOnUIThread
         new Thread(()->{
+            //建立連線
+            OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                    //加入攔截器 可以印出request的訊息                        //level.BASIC = 印出request和response
+                    .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
+                    .build();
+            //設定傳送request
+            Request request = new Request.Builder()
+                    .url(data)
+                    .build();
+            //設定回傳
+            Call call = okHttpClient.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    System.out.println("okhttp傳送失敗"+e);
+                }
 
-                OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
-                        .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
-                        .build();
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+//                        System.out.println("回傳"+response.body().string());   //測試抓資料OK
+                    assert response.body() != null;
+                    String allData = response.body().string();
 
-                Request request = new Request.Builder()
-                        .url(data)
-                        .build();
+                    try {
+                        //把String轉成JSONArray
+                        JSONArray jsonArray = new JSONArray(allData);
+                        //建一個StringBuilder的物件(SB相較於String更靈活)
+                        StringBuilder titleName= new StringBuilder();
 
-                Call call = okHttpClient.newCall(request);
-                call.enqueue(new Callback() {
-                    @Override
-                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                        System.out.println("okhttp傳送失敗"+e);
-                    }
+                        //用迴圈取出jsonArray的值
+                        for (int i=0; i<jsonArray.length(); i++){
+                            //把JSONArray裡每個JSONObject取出來
+                            JSONObject obj = jsonArray.getJSONObject(i);
+                                            //用key取得JSONObject的值
+                            String title = obj.getString("title");
 
-                    @Override
-                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-//                        System.out.println("回傳"+response.body().string());   //OK
-                        assert response.body() != null;
-                        String allData = response.body().string();
-
-                        try {
-                            JSONArray jsonArray = new JSONArray(allData);
-                            for (int i=0; i<jsonArray.length(); i++){
-                                JSONObject obj = jsonArray.getJSONObject(i);
-                                String title = obj.getString("title");
-                                StringBuilder titleName= new StringBuilder();
-                                titleName.append(title).append("\n");
-
-                                runOnUiThread(()->{
-                                    binding.textView.setText(title);
-                                });
-                            }
-//
-
-
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
+                            titleName.append(title).append("\n");
                         }
+
+                        runOnUiThread(()->{
+                            binding.textView.setText(titleName);
+                        });
+
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
                     }
-                });
-
-
-
+                }
+            });
 
         }).start();
     }
 
+    private void catch_data2(){
+        String data = "https://jsonplaceholder.typicode.com/posts";
+        new Thread(()->{
+            //建立連線
+            OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                    //加入攔截器 可以印出request的訊息                        //level.BASIC = 印出request和response
+                    .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
+                    .build();
+            //設定傳送request
+            Request request = new Request.Builder()
+                    .url(data)
+                    .build();
+            //設定回傳
+            Call call = okHttpClient.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    System.out.println("okhttp傳送失敗"+e);
+                }
 
-//    public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
-//
-//        ArrayList<HashMap<String, String>> arrayList;
-//        LayoutInflater inflater;
-//
-//        public class ViewHolder extends RecyclerView.ViewHolder{
-//            TextView txtUserID, txtAlbumTitle, textView3, textView4;
-//            public ViewHolder(@NonNull View itemView) {
-//                super(itemView);
-//                txtUserID = itemView.findViewById(R.id.txtUserID);
-//                txtAlbumTitle = itemView.findViewById(R.id.txtAlbumTitle);
-//                textView3 = itemView.findViewById(R.id.textView3);
-//                textView4 = itemView.findViewById(R.id.textView4);
-//            }
-//        }
-//
-//        public Adapter(ArrayList<HashMap<String, String>> arrayList, Context context) {
-//            this.arrayList = arrayList;
-//            inflater = LayoutInflater.from(context);
-//        }
-//
-//        @NonNull
-//        @Override
-//        public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.data_item, parent, false);
-//            return new ViewHolder(view);
-//        }
-//
-//        @Override
-//        public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
-//            holder.txtUserID.setText(arrayList.get(position).get("userId"));
-//            holder.txtAlbumTitle.setText(arrayList.get(position).get("title"));
-//            holder.textView3.setText("UserID:");
-//        }
-//
-//        @Override
-//        public int getItemCount() {
-//            return arrayList.size();
-//        }
-//    }
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+//                        System.out.println("回傳"+response.body().string());   //測試抓資料OK
+                    assert response.body() != null;
+                    String allData = response.body().string();
+
+                    //建一個gson物件
+                    Gson gson = new Gson();
+                                        //TypeToken: gson的資料類型轉換器, 先用TypeToken<>得到匿名內部類別, 再由該匿名內部類別成員使用.getType()得到要轉換的Type
+                                        //
+                                        //解釋By: https://blog.csdn.net/jiangyu1013/article/details/56489412
+                    Type myDataType = new TypeToken<List<myData>>() {}.getType();
+                                            //把JSON字串轉成Type(aka List<myData>)
+                    List<myData> dataList = gson.fromJson(allData, myDataType);
+
+                    System.out.println(dataList);
+                    StringBuilder bodyName= new StringBuilder();
+                    for (myData myData: dataList){
+                        String body = myData.getBody();
+                        bodyName.append(body).append("\n");
+                    }
+
+                    runOnUiThread(()->{
+                        binding.textView.setText(bodyName);
+
+                    });
+
+
+                }
+            });
+
+        }).start();
+    }
+
 
 }
